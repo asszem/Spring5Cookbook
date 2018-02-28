4462d80846a8b3940d5cb2d8df158e9de38f8e12
Andras Olah <andras.antal.olah@hu.ibm.com>
2018-02-28 15:00:08 +0100

    Update ch2-xml with missing files, web.xml template from downloaded source code

---
+++ ch02-jc/src/main/java/SpringWebInitializer.java
@@ -0,0 +1,13 @@
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class SpringWebInitializer  implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
