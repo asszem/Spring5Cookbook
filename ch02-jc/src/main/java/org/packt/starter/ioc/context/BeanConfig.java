fc6c000ab764d6fcf2ff4f2b402d9753eee6ef5f
Andras Olah <andras.antal.olah@hu.ibm.com>
2018-03-01 12:14:12 +0100

    Add missing files to ch02-jc from downloaded source code

---
+++ ch02-jc/src/main/java/org/packt/starter/ioc/context/BeanConfig.java
@@ -0,0 +1,7 @@
+package org.packt.starter.ioc.context;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+}
--- ch02-jc/src/main/java/org/packt/starter/ioc/webxml/SpringWebInitializer.java
+++ ch02-jc/src/main/java/org/packt/starter/ioc/webxml/SpringWebInitializer.java
@@ -1,13 +1,14 @@
+package org.packt.starter.ioc.webxml;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.springframework.web.WebApplicationInitializer;
 
-public class SpringWebInitializer  implements WebApplicationInitializer{
+public class SpringWebInitializer implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext arg0) throws ServletException {
 		// TODO Auto-generated method stub
 	}
-
-
+	  
 }
